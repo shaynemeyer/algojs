a4e626d16445266a916aeee504654daa21173eb1
Shayne Meyer <shayne.meyer@gmail.com>
2018-01-10 12:27:28 -0800

    finished is palindrome

---
+++ IsPalindrome/palindrome.js
@@ -0,0 +1,22 @@
+function isPalindrome(str) {
+  str = str.toLowerCase();
+  var charactersArr = str.split('');
+  var validCharacters = 'abcdefghijklmnopqrstuvwxy'.split('');
+
+  var lettersArr = [];
+  charactersArr.forEach(char => {
+    if(validCharacters.indexOf(char) > -1) {
+      lettersArr.push(char);
+    }
+  });
+
+  if(lettersArr.join('') === lettersArr.reverse().join('')) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isPalindrome("Madam I'm Adam"));
+console.log(isPalindrome("race car"));
+console.log(isPalindrome("Coding JavaScript"));
