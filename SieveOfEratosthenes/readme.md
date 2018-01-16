adc6208013eb1ae610633ce18aa90f9298847049
Shayne Meyer <shayne.meyer@gmail.com>
2018-01-16 07:40:34 -0800

    Added Sieve of Eratosthenes

---
+++ SieveOfEratosthenes/readme.md
@@ -0,0 +1,7 @@
+# Sieve of Eratosthenes
+Return all prime numbers up to 'num' in an array.
+
+```JavaScript
+sieveOfEratosthenes(20);
+// should return [2, 3, 5, 7, 11, 13, 17, 19]
+```
---
+++ SieveOfEratosthenes/sieve.js
@@ -0,0 +1,27 @@
+function sieveOfEratosthenes(num) {
+  var primes = [];
+  for(var i = 0; i <= num; i++){
+    primes[i] = true;
+  }
+
+  primes[0] = false;
+  primes[1] = false;
+
+  for(var i = 2; i <= Math.sqrt(num); i++){
+    for(var j = 2; j * i <= num; j++) {
+      primes[i*j] = false;
+    }
+  }
+
+  var result = [];
+  for(var i = 0; i < primes.length; i++){
+    if(primes[i]) {
+      result.push(i);
+    }
+  }
+
+  return result;
+}
+
+console.log(sieveOfEratosthenes(10));
+console.log(sieveOfEratosthenes(20));
